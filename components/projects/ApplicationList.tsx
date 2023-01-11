2f7923151f9a6cf46e18b27f9536031f59b43973
natnaelawel <natnael.awel@gmail.com>
2023-01-11 22:27:08 +0300

    fix(web): build

--- components/projects/ApplicationList.tsx
+++ components/projects/ApplicationList.tsx
@@ -64,7 +64,7 @@ const ApplicationList = (props: Props) => {
             </div>
             <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-5">
                 {
-                    applications.map((application: any, index: any) => {
+                    applications && applications.map((application: any, index: any) => {
                         return <ApplicationItem key={application.uid} application={application} />
                     })
                 }
